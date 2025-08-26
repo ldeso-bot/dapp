2108490f4a3abac2ab2561aed5a4ddc7c37e7a1b
biwano <biwano@gmail.com>
2025-08-26 19:55:01 +0200

    Fix endpoints

--- src/shared/constants/subgraph.constants.ts
+++ src/shared/constants/subgraph.constants.ts
@@ -3,14 +3,14 @@ import { base, baseSepolia } from 'viem/chains';
 export const subgraphs = {
   [base.id]: {
     carbon:
-      'https://api.studio.thegraph.com/query/88565/biwano-polygon-digital-carbon/version/latest',
+      'https://api.studio.thegraph.com/query/78559/polygon-digital-carbon/version/latest',
     protocol:
-      'https://api.studio.thegraph.com/query/88565/biwano-carbonmark/version/latest',
+      'https://api.studio.thegraph.com/query/78559/klimadao-carbonmark/version/latest',
   },
   [baseSepolia.id]: {
     carbon:
-      'https://api.studio.thegraph.com/query/88565/biwano-polygon-digital-carbon/version/latest',
+      'https://api.studio.thegraph.com/query/78559/polygon-digital-carbon/version/latest',
     protocol:
-      'https://api.studio.thegraph.com/query/88565/biwano-carbonmark/version/latest',
+      'https://api.studio.thegraph.com/query/78559/klimadao-carbonmark/version/latest',
   },
 };
