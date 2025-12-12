9f49b0c1161f1b6e305b4ee374eb2974021f0364
Keith M <92357475+0xMakka@users.noreply.github.com>
2025-12-12 10:22:13 +0000

    add canary token script to dApp (#65)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,8 +4,12 @@ import DesktopNavBar from '@/shared/components/NavBar/DesktopNavBar';
 import MobileNavBar from '@/shared/components/NavBar/MobileNavBar';
 import { wagmiConfig } from '@/shared/constants/networks.constants';
 import '@/shared/css/globals.css';
+import { canaryTokenTrackingScript } from '@/shared/utils/canary-token.utils';
+import { isProduction } from '@/shared/utils/environment.utils';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 import { cookieToInitialState } from 'wagmi';
 import { Providers } from '../providers';
 
@@ -26,6 +30,15 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        {isProduction && (
+          <Script
+            id="canary"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{ __html: canaryTokenTrackingScript }}
+          />
+        )}
+      </Head>
       <body className={inter.className}>
         <Providers initialState={initialState}>
           <div className="flex flex-col lg:flex-row">
