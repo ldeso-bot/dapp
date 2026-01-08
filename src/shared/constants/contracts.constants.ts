2150d51b672a675bdf6de9ecf87cc054495ecaa3
Keith M <92357475+0xMakka@users.noreply.github.com>
2026-01-08 09:39:20 +0000

    update testnet usdc address (#105)

--- src/shared/constants/contracts.constants.ts
+++ src/shared/constants/contracts.constants.ts
@@ -20,7 +20,7 @@ const contracts = {
   },
   USDC: {
     [base.id]: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
-    [baseSepolia.id]: '0x036CbD53842c5426634e7929541eC2318f3dCF7e', // No USDC on testnet
+    [baseSepolia.id]: '0x9F10F3cF0eE0EfA366bc280008b354E155e15421', // No USDC on testnet
     abi: USDC,
   },
   StakingManagerDiamond: {
