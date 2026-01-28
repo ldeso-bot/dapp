c87a4e2a4fa58d9f5d853a1b452f56973fd5ffa4
Keith M <92357475+0xMakka@users.noreply.github.com>
2026-01-28 07:15:15 +0000

    add z-index to backdrop (#164)

--- src/shared/components/Dialog/Dialog.tsx
+++ src/shared/components/Dialog/Dialog.tsx
@@ -24,7 +24,7 @@ export default function Dialog(props: Props) {
   return (
     <DialogPrimitive.Root open={open}>
       <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay className="fixed inset-0 bg-overlay" />
+        <DialogPrimitive.Overlay className="fixed inset-0 bg-overlay z-50" />
         <DialogPrimitive.Content
           className={cn(
             'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 w-fit',
