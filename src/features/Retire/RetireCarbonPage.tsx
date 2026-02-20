db72173eb666fba1ed8be340fc09f4239903f8ba
0xtapi <128430614+0xtapi@users.noreply.github.com>
2026-02-20 13:21:49 +0100

    Fix retire page CTA behavior (#365)
    * Replace kVCM check with address check
    
    * Knip

--- src/features/Retire/RetireCarbonPage.tsx
+++ src/features/Retire/RetireCarbonPage.tsx
@@ -5,21 +5,20 @@ import { PageDescription } from '@/shared/components/PageDescription/PageDescrip
 import { PageTitle } from '@/shared/components/PageTitle/PageTitle';
 import Steps from '@/shared/components/Steps/Steps';
 import { CARBONMARK_URL } from '@/shared/constants/urls.constants';
-import { useWalletData } from '@/shared/hooks/api/useWalletData';
 import { useParsedForm } from '@/shared/hooks/web3/useParsedForm';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { RetireCarbonEmptyState } from './components/RetireCarbonEmptyState';
+import { useAccount } from 'wagmi';
 import { RegenNetworkCreditsCard } from './components/RegenNetworkCreditsCard';
+import { RetireCarbonEmptyState } from './components/RetireCarbonEmptyState';
 import { RetireCarbonFields, retireCarbonSchema } from './retire.constants';
 import RetireCarbonConfirm from './steps/RetireCarbonConfirm';
 import RetireCarbonForm from './steps/RetireCarbonForm';
 
 export default function RetirePage() {
-  const { data: walletData } = useWalletData();
+  const { address } = useAccount();
+  const showEmptyState = !address;
   const schema = retireCarbonSchema;
-
-  const showEmptyState = (walletData?.balances.kvcm ?? 0) == 0;
 
   const form = useForm<RetireCarbonFields>({
     resolver: zodResolver(schema),
@@ -37,42 +36,42 @@ export default function RetirePage() {
 
   const parsedForm = useParsedForm(form, schema);
 
+  if (showEmptyState) {
+    return <RetireCarbonEmptyState />;
+  }
+
   return (
-    <>
-      {showEmptyState ? (
-        <RetireCarbonEmptyState />
-      ) : (
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-2">
-            <PageTitle>Retire Carbon</PageTitle>
-            <PageDescription>
-              Purchase a carbon offset and receive a receipt with proof of
-              retirement instantly. Powered by{' '}
-              <a
-                href={CARBONMARK_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium text-primary-600 hover:underline"
-              >
-                Carbonmark
-              </a>
-              , our partner in carbon retirements.
-            </PageDescription>
-          </div>
-          <div className="flex gap-4 space-between mx-auto">
-            <Steps
-              components={[RetireCarbonForm, RetireCarbonConfirm]}
-              data={{ form, schema, parsedForm }}
-            />
-            <div className="flex flex-col gap-4">
-              <CarbonClassCard />
-              <RegenNetworkCreditsCard
-                selectedCarbonClassId={form.watch('carbonClass')}
-              />
-            </div>
-          </div>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <PageTitle>Retire Carbon</PageTitle>
+        <PageDescription>
+          Purchase a carbon offset and receive a receipt with proof of
+          retirement instantly. Powered by{' '}
+          <a
+            href={CARBONMARK_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium text-primary-600 hover:underline"
+          >
+            Carbonmark
+          </a>
+          , our partner in carbon retirements.
+        </PageDescription>
+      </div>
+
+      <div className="flex gap-4 space-between mx-auto">
+        <Steps
+          components={[RetireCarbonForm, RetireCarbonConfirm]}
+          data={{ form, schema, parsedForm }}
+        />
+
+        <div className="flex flex-col gap-4">
+          <CarbonClassCard />
+          <RegenNetworkCreditsCard
+            selectedCarbonClassId={form.watch('carbonClass')}
+          />
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
--- src/features/Retire/components/RetireCarbonEmptyState.tsx
+++ src/features/Retire/components/RetireCarbonEmptyState.tsx
@@ -2,11 +2,8 @@
 
 import { ImportantToKnow } from '@/features/Retire/components/ImportantToKnow';
 import { EmptyState } from '@/shared/components/EmptyState/EmptyState';
-import { DollarIcon } from '@/shared/components/Svg/DollarIcon';
 import { WalletIcon } from '@/shared/components/Svg/WalletIcon';
-import { BUY_KVCM_URL } from '@/shared/constants/urls.constants';
 import { CtaConfig } from '@/shared/utils/emptyState.utils';
-import { useAccount } from 'wagmi';
 import {
   retireCarbonFlowItems,
   retireCarbonInfoCards,
@@ -14,22 +11,12 @@ import {
 } from '../retire.constants';
 
 export const RetireCarbonEmptyState = () => {
-  const { address } = useAccount();
-
-  const emptyStateButtonConfig: CtaConfig = !address
-    ? {
-        text: 'Connect wallet to start',
-        icon: WalletIcon,
-        description: 'You choose the credits and set the beneficiary details.',
-        onClick: (openConnectModal) => openConnectModal(),
-      }
-    : {
-        text: 'Get kVCM',
-        icon: DollarIcon,
-        description:
-          'Acquire kVCM tokens to offset emissions and retire credits.',
-        href: BUY_KVCM_URL,
-      };
+  const emptyStateButtonConfig: CtaConfig = {
+    text: 'Connect wallet to start',
+    icon: WalletIcon,
+    description: 'You choose the credits and set the beneficiary details.',
+    onClick: (openConnectModal) => openConnectModal(),
+  };
 
   return (
     <EmptyState
