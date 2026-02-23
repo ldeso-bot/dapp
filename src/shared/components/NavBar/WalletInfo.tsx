eb968680ba03ee61fc9d5481f46ee09bea068362
Keith M <92357475+0xMakka@users.noreply.github.com>
2026-02-23 12:54:44 +0000

    Remove chain toggle (#358)
    * remove chain switch between testnet and mainnet
    
    * enable DEV_MODE switcher
    
    * Show network switch text only in DEV_MODE
    
    ---------
    
    Co-authored-by: biwano <11857992+biwano@users.noreply.github.com>

--- src/shared/components/NavBar/WalletInfo.tsx
+++ src/shared/components/NavBar/WalletInfo.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import {
-  DEFAULT_TO_TESTNET,
-  DEV_MODE,
-} from '@/shared/constants/config.constants';
+import { DEV_MODE } from '@/shared/constants/config.constants';
+import { cn } from '@/shared/utils/component.utils';
 import { formatAddress } from '@/shared/utils/string.utils';
 import { base, baseSepolia } from 'viem/chains';
 import { useAccount, useSwitchChain } from 'wagmi';
@@ -14,16 +12,14 @@ export default function WalletInfo() {
   const { switchChain } = useSwitchChain();
 
   const chainText =
-    chain == baseSepolia
+    chain === baseSepolia
       ? 'Connected to testnet'
-      : chain == base
-        ? DEV_MODE || DEFAULT_TO_TESTNET
-          ? 'Connected to mainnet'
-          : ''
+      : chain === base
+        ? 'Connected to mainnet'
         : 'Unsupported chain. Please switch to Base.';
 
   const handleSwitchChain = () => {
-    switchChain({ chainId: chain == base ? baseSepolia.id : base.id });
+    switchChain({ chainId: chain === base ? baseSepolia.id : base.id });
   };
 
   if (!address) return null;
@@ -34,12 +30,17 @@ export default function WalletInfo() {
       <div className="py-5">
         <div>My Wallet Address:</div>
         <div className="text-void-40">{`${formatAddress(address)}`}</div>
-        <div
-          className="text-void-40 text-sm text-red cursor-pointer"
-          onClick={handleSwitchChain}
-        >
-          {chainText}
-        </div>
+        {DEV_MODE && (
+          <div
+            className={cn(
+              'text-void-40 text-size-12',
+              DEV_MODE ? 'cursor-pointer' : ''
+            )}
+            onClick={handleSwitchChain}
+          >
+            {chainText}
+          </div>
+        )}
       </div>
       <Separator />
     </div>
