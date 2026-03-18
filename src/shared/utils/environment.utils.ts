d9b5ef58600f6716ba347cbd3d2143698979a4ca
biwano <11857992+biwano@users.noreply.github.com>
2026-03-18 08:49:29 +0100

    fix: update isProduction check for stricter VERCEL_ENV comparison (#458)

--- src/shared/utils/environment.utils.ts
+++ src/shared/utils/environment.utils.ts
@@ -2,7 +2,8 @@ import { base, baseSepolia } from 'viem/chains';
 import { IS_DEVELOPMENT } from '../constants/config.constants';
 
 export const isProduction =
-  process.env.NODE_ENV === 'production' && process.env.VERCEL_ENV !== 'preview';
+  process.env.NODE_ENV === 'production' &&
+  process.env.VERCEL_ENV === 'production';
 
 const isPreview = () =>
   process.env.VERCEL_ENV === 'preview' ||
